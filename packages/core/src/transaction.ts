4e7a0bbc38301a8a57b0211a70eb290e908c6913
Dohyu <hdhcoss@gmail.com>
2025-05-23 03:15:01 +0900

    feat: implement transaction

--- packages/core/src/transaction.ts
+++ packages/core/src/transaction.ts
@@ -1,1 +1,33 @@
-export const transaction = (payload, fn) => {};
+import type {
+	Identified,
+	PayloadOf,
+	StorageValue,
+	TransactionalClient,
+	TransactionCallback,
+} from "./types";
+import { identifyClient } from "./utils/identify";
+
+export const transaction = async <
+	Client extends TransactionalClient<Tx>,
+	Tx extends Record<string, unknown>,
+	R,
+>(
+	payload: PayloadOf<Client, Tx>,
+	fn: TransactionCallback<Identified<Tx>, R>,
+): Promise<R> => {
+	const store = payload.storage.getStore();
+
+	if (store) {
+		return fn(store.transaction);
+	}
+
+	const task = payload.client._transaction((tx) => {
+		const _store: StorageValue<Client, Tx> = {
+			transaction: identifyClient(tx),
+		};
+
+		return payload.storage.run(_store, () => fn(_store.transaction));
+	});
+
+	return task;
+};
