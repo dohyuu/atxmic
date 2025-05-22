dbe9c6e19da11e05f677e329c644d9513c857d0e
Dohyu <hdhcoss@gmail.com>
2025-05-23 04:51:46 +0900

    test: add test cases for proxy

---
+++ packages/core/src/proxy.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it } from "vitest"
+
+describe("createProxy", () => {
+  it("should use existing transaction", async () => {})
+  it("should use new transaction", async () => {})
+  it("should use stored transaction in transaction", async () => {})
+})
