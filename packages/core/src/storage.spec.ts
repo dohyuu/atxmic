aaad7e2f84373d75027815a344d5ae71d343eae5
Dohyu <hdhcoss@gmail.com>
2025-05-23 03:19:03 +0900

    feat: implement storage

---
+++ packages/core/src/storage.spec.ts
@@ -0,0 +1,10 @@
+import { AsyncLocalStorage } from "node:async_hooks";
+import { describe, expect, it } from "vitest";
+import { createStorage } from "./storage";
+
+describe("storage", () => {
+	it("should create storage", () => {
+		const storage = createStorage();
+		expect(storage).toBeInstanceOf(AsyncLocalStorage);
+	});
+});
--- packages/core/src/storage.ts
+++ packages/core/src/storage.ts
@@ -1,5 +1,21 @@
 import { AsyncLocalStorage } from "node:async_hooks";
+import type {
+	StorageValue,
+	TransactionalClient,
+	TransactionalStorage,
+	TransactionOf,
+} from "./types";
 
-export const createStorage = () => {
-	return new AsyncLocalStorage();
+export class TransactionStorage<
+		Client extends TransactionalClient<Tx>,
+		Tx extends Record<string, unknown> = TransactionOf<Client>,
+	>
+	extends AsyncLocalStorage<StorageValue<Client, Tx>>
+	implements TransactionalStorage<Client, Tx> {}
+
+export const createStorage = <
+	Client extends TransactionalClient<Tx>,
+	Tx extends Record<string, unknown> = TransactionOf<Client>,
+>(): TransactionalStorage<Client, Tx> => {
+	return new TransactionStorage<Client, Tx>();
 };
