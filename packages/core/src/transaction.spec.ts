a7f0655f20f03501e2645a29b6f67e90f635ceff
Dohyu <hdhcoss@gmail.com>
2025-05-22 21:22:52 +0900

    test: transaction

---
+++ packages/core/src/transaction.spec.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from "vitest";
+
+describe("transaction", () => {
+	it("should run with new transaction", async () => {
+	});
+
+	it("should use new transaction when no stored transaction", async () => {
+	});
+
+	it("should use stored transaction", async () => {
+	});
+
+	it("should use same transaction in nested calls", async () => {
+	});
+
+	it("should use different transaction", async () => {
+	});
+});
