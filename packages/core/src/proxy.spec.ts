bb6c2e2f37ec92711bf9d2f606cda42dd4c78332
Dohyu <hdhcoss@gmail.com>
2025-05-23 05:17:01 +0900

    test: internalTransaction

--- packages/core/src/proxy.spec.ts
+++ packages/core/src/proxy.spec.ts
@@ -1,4 +1,6 @@
-import { describe, it } from "vitest"
+import { describe, expect, it } from "vitest"
+import { internalTransaction } from "./proxy"
+import { createTestTx } from "./utils/test"
 
 describe("createProxy", () => {
   it("should use existing transaction", async () => {})
@@ -7,8 +9,45 @@ describe("createProxy", () => {
 })
 
 describe("internalTransaction", () => {
-  it("should handle single promise", async () => {})
-  it("should handle multiple promises", async () => {})
-  it("should handle function", async () => {})
-  it("should throw error if not a promise or function", async () => {})
+  it("should return handler", () => {
+    const tx = createTestTx()
+    const handler = internalTransaction(tx)
+    expect(handler).toBeInstanceOf(Function)
+  })
+
+  it("should handle single promise", async () => {
+    const tx = createTestTx()
+    const handler = internalTransaction(tx)
+    const result = await handler(Promise.resolve(1))
+    expect(result).toEqual([1])
+  })
+
+  it("should handle array of promises", async () => {
+    const tx = createTestTx()
+    const handler = internalTransaction(tx)
+    const result = await handler([Promise.resolve(1), Promise.resolve(2)])
+    expect(result).toEqual([1, 2])
+  })
+
+  it("should handle multiple promises", async () => {
+    const tx = createTestTx()
+    const handler = internalTransaction(tx)
+    const result = await handler(Promise.resolve(1), Promise.resolve(2))
+    expect(result).toEqual([1, 2])
+  })
+
+  it("should handle function", async () => {
+    const tx = createTestTx()
+    const handler = internalTransaction(tx)
+    const result = await handler(async (tx) => {
+      return tx.find()
+    })
+    expect(result).toBe("find")
+  })
+
+  it("should throw error if not a promise or function", async () => {
+    const tx = createTestTx()
+    const handler = internalTransaction(tx)
+    await expect(handler(1 as any)).rejects.toThrow()
+  })
 })
